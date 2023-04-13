56e221b980e33da21faceec5ff89b6efbee3da07
kzsy <kzamani44@gmail.com>
2023-04-13 16:34:00 +0100

    still working

--- js/main.js
+++ js/main.js
@@ -3,16 +3,14 @@ const navMenu = document.querySelector('.nav-menu');
 const navBar = document.querySelector('.navbar');
 const signBtn = document.querySelector('.sign');
 const subscibeBtn = document.querySelector('#contact-btn');
+const signLink = document.querySelector('.nav-btn');
 
 // Show menu When Nav Button is clicked on Mobile Devices
 hamburgerMenu.addEventListener('click', (e) => {
 	hamburgerMenu.classList.toggle('active');
+	signBtn.appendChild(signLink);
 	navMenu.classList.toggle('active');
 	navMenu.appendChild(signBtn);
 });
 
 // Remove Sign-in Button from NavBar if ScreenWidth is < 500px
-const mobileScreen = screen.width;
-if (mobileScreen <= 500) {
-	navBar.removeChild(signBtn);
-}
